3b3c14b2edd907b405d3c7fc42cf487d08287185
anandmann9496 <jes6anand@gmail.com>
2025-04-19 23:04:30 +0530

    Add files via upload

---
+++ Try Fade.js
@@ -0,0 +1,24 @@
+
+
+function(e){
+    if(!e.document)throw new Error("jQuery requires a window with a document");return t(e)}:t(e)}("undefined"!=typeof window?window:this,function(C,e){"use strict";var t=[],r=Object.getPrototypeOf,s=t.slice,g=t.flat?function(e){return t.flat.call(e)}
+
+
+
+S.extend({cssHooks:{
+    opacity:{get:function(e,t){
+        if(t){var n=We(e,"opacity");return""===n?"1":n}}}}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
